723a1a35ee061542960dfbe2d49e9d3adf8803ff
foroozanakbarpour <142745661+foroozanakbarpour@users.noreply.github.com>
2024-05-13 13:45:29 +0330

    Update dataSQlite.py

--- dataSQlite.py
+++ dataSQlite.py
@@ -1,52 +1,57 @@
 import sqlite3
 from sys import path
+import bcrypt
 #  path[0] for one of vs codes problem doestn dopend on program
 
 def create_table():
-    curosr .execute('CREATE TABLE IF NOT EXISTS users_table(password INTEGER PRIMARY KEY , username TEXT , email TEXT , phone_number INTEGER )') 
+    cursor .execute('CREATE TABLE IF NOT EXISTS users_table(password TEXT PRIMARY KEY , username TEXT , email TEXT , phone_number INTEGER )') 
 
 
-def insert_info(*args) :
+def insert_info(password , user_name , email , phone_number) :
     try :
-        curosr .execute('INSERT INTO users_table VALUES(?,?,?,?)' , (*args,))
+        password = b"password"
+        salt = bcrypt .gensalt()
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+        cursor .execute('INSERT INTO users_table VALUES(?,?,?,?)' , (hashed_password , user_name , email , phone_number))
         database .commit()
     except :
-        raise sqlite3 .IntegrityError ('password must be 6 digit uniq number')
+        raise sqlite3 .IntegrityError ('password must be with len 8 with at least one number and one letter')
     return True
 
 # query with password i mean primary key
 def users_info(password) :   
-  users= curosr .execute('SELECT * FROM users_table WHERE password = ?' , (password ,))
+  users = cursor .execute('SELECT * FROM users_table WHERE password = ?' , (password,)) .fetchone()
   if users == None :
       raise ValueError('password as primary key dosent exist!')
+  print(users)
   return users
 
 # query with email
 def read_info(email) : 
-   emails = curosr .execute("SELECT * FROM users_table WHERE email = ?" , (email ,))
-   if emails == None :
+   emails = cursor .execute("SELECT * FROM users_table WHERE email = ?" , (email ,)) .fetchone()
+   if emails is None :
       raise ValueError('email dosent exist!')
    return emails
 
 # query with phone number
 def give_info(phone_number) :
-    phoneNumber =curosr .execute("SELECT * FROM users_table WHERE phone_number = ? " , (phone_number,))
+    phoneNumber = cursor .execute("SELECT * FROM users_table WHERE phone_number = ? " , (phone_number,)) .fetchone()
     if phoneNumber == None :
         raise ValueError ('phone number dosent exist!')
     return True
 
 
 def delete_fired_user(password) :
-    curosr .execute('DELETE FROM users_table WHERE password = ?' , (password ,))
+    cursor .execute('DELETE FROM users_table WHERE password = ?' , (password ,))
     database . commit()
-# for somebody who wants to change his or her data
+    
 def update_info(password , username , email , phone_number) :
-    curosr .execute('UPDATE users_table SET username = ? , email = ? , phone_number = ? WHERE password = ?' , (username , email , phone_number , password))
+    cursor .execute('UPDATE users_table SET username = ? , email = ? , phone_number = ? WHERE password = ?' , (username , email , phone_number , password))
     database . commit()
-
 
 # main-------------------------------------------------------
 
 database = sqlite3 .connect(path[0] +"/data.db")
-curosr = database . cursor()
+cursor = database . cursor()
 database .commit()
+
