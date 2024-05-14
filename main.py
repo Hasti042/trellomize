39eb09fbfd917c373a3c5cb4f3741e074548f435
Hasti <168625645+Hasti042@users.noreply.github.com>
2024-05-14 18:44:12 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+from rich.console import Console
+console = Console()
+class user:
+    def __init__(self,email,username,password):
+        self.email=email
+        self.username=username
+        self.password=password
+        self.active=True
+
+class Account:
+   def __init__(self):
+        self.users={}
+   
+   def registeration(self,email,username,password):
+       if email in self.users or password in self.users:
+           console.print("username or password have been used before",style="bold red")
+       self.users[username]=user(email,username,password)
+       console.print("you have successfully registered",style="bold green")
+    
+   def login(self,username,password):
+       if username not in self.users:
+           console.print("the username does not exist",style="bold red")
+           return
+       if  self.users[username].password !=password:
+           console.print("the password is wrong",style="bold red")
+           return
+       if not self.users[username].active:
+           console.print("your account is deactive!",style="bold red")
+           return
+   
+   def inactive(self,username):
+       if username not in self.users:
+           console.print("the username does not exist",style="bold red")
+           return
+       self.users[username].active=False
+ 
+       
+       
