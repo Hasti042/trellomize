ecbc5570645a887b9bcd36a9e31ce8ec5bd9f0fa
foroozanakbarpour <142745661+foroozanakbarpour@users.noreply.github.com>
2024-05-14 11:35:12 +0330

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,23 +1,26 @@
 # ****************************************************** all of this for menu becouse wo havent got any web page or app and we working in cmd 
-import loging as lo
+import log_in as lo
 while True :
     
     lo .clear()
+    print('havent you got any account ? you have to regester at first press R to Register ')
     print('press L to login')
-    print('press R to register')
     print('press C to change password or you forget your password')
     print('press Q to quite application')
+
     choice = input('Enter your choice :').upper()
     if choice == 'L':
-        lo .users_pass()
+        lo .Loging_System.logging()
     elif choice == 'R':
-        lo .make_password()
+        lo .Register()
     elif choice == 'C':
-        lo .change_password()
+        lo .Loging_System .change_password()
     elif choice == 'Q':
         break
     else :
         print('Wrong choice !')
+    
+    input('press any key to continue ....')  
 
 
 #  this is goingo to be complete soon ..
