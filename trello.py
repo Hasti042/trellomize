1fad7600b67fc9c00d9ea992fdae53c6e7153059
Hasti <168625645+Hasti042@users.noreply.github.com>
2024-05-20 22:07:33 +0330

    Create trello.py

---
+++ trello.py
@@ -0,0 +1,16 @@
+import uuid
+from datetime import datetime,timedelta
+from enum import Enum
+
+class Priority(Enum):
+    CRITICAL = 1
+    HIGH = 2
+    MEDIUM = 3
+    LOW = 4
+
+class Status(Enum):
+    BACKLOG = 1
+    TODO = 2
+    DOING = 3
+    DONE = 4
+    ARCHIVED = 5
