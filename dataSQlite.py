c9c8a17a249f507f89d87458e4f671ce16fadda3
foroozanakbarpour <142745661+foroozanakbarpour@users.noreply.github.com>
2024-05-23 12:39:44 +0330

    Update dataSQlite.py

--- dataSQlite.py
+++ dataSQlite.py
@@ -5,15 +5,14 @@ import bcrypt
 database = sqlite3 .connect(path[0] +"/data.db")
 cursor = database . cursor()
 
-
+# users table
 def create_table():
     cursor .execute('CREATE TABLE IF NOT EXISTS users_table(password TEXT PRIMARY KEY , username TEXT , email TEXT , phone_number INTEGER )') 
 
-
-
 def insert_info(password , user_name , email , phone_number) :
     try :
-        cursor .execute('INSERT INTO users_table VALUES(?,?,?,?)' , (password , user_name , email , phone_number))
+        cursor .execute('INSERT INTO users_table VALUES(?,?,?,?)' , 
+                       (password , user_name , email , phone_number))
         database .commit()
     except sqlite3 .IntegrityError :
            print('Password must be unique.') 
@@ -22,14 +21,16 @@ def insert_info(password , user_name , email , phone_number) :
 
 # query with password i mean primary key
 def verify_password(password , user_name) :   
-  users = cursor .execute('SELECT username , password FROM users_table WHERE password = ? AND username = ?' , (password,user_name)) .fetchone()
+  users = cursor .execute('SELECT username , password FROM users_table WHERE password = ? AND username = ?' ,
+                         (password,user_name)) .fetchone()
   if users == None :
       raise ValueError('password as primary key dosent exist!')
   return users
 
 # query with email
 def read_info(email) : 
-   emails = cursor .execute("SELECT * FROM users_table WHERE email = ?" , (email ,)) .fetchone()
+   emails = cursor .execute("SELECT * FROM users_table WHERE email = ?" , 
+                           (email ,)) .fetchone()
    if emails is None :
       raise ValueError('email dosent exist!')
    return emails
@@ -50,10 +51,42 @@ def update_info(password , username , email , phone_number) :
     cursor .execute('UPDATE users_table SET username = ? , email = ? , phone_number = ? WHERE password = ?' , (username , email , phone_number , password))
     database . commit()
 
+# ********************************************************************
+# superusers table
 
-# main-------------------------------------------------------
-database .commit()
-database .close()
+def creat_admins_table():
+    cursor .execute('CREATE TABLE IF NOT EXISTS admins_table ( username TEXT , password TEXT PRIMARY KEY  )')
+
+
+def insert_admin_data(username , password):
+    try :
+       cursor .execute("INSERT INTO admins_tabel VALUES (?,?) " , (username , password))
+       database .commit()
+    except :
+        sqlite3 .IntegrityError('Admin with this data already exist !')
+        return False
+    return True
+
+def autuntication(username , password) :
+    user=cursor .execute('SELECT * FROM admins_table WHERE username = ? , password = ?' , (username , password)) .fetchone()
+    if user ==None :
+        print('there isnt any admins with this data !')
+    return user
+
+
+
+if __name__ == '__main__' :
+
+   
+
+
+
+    database .commit()
+    # database .close()
+
+
+
+
 
 
 
